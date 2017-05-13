7dc2b278aa4476a0bb0c58450de4b2b8a683a851
Janice Yura Lee <janiceyuralee@gmail.com>
2017-05-13 16:30:04 -0700

    Add skeletons

--- frontend/components/app.js
+++ frontend/components/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import PostContainer from './post_container';
 
 const App = () => (
   <div>
     <h1>Assessment 06 - by Janice</h1>
-    <PostContainer />
+
   </div>
 );
 
